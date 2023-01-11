e5213d0339ddcbb7d72f38b390ebb27cc9fc3a7b
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2023-01-11 16:23:24 +0000

    Reformat the tracks example with rustfmt

--- examples/tracks.rs
+++ examples/tracks.rs
@@ -1,13 +1,17 @@
 // Simple program to list track numbers and logical sector numbers of
 // a Compact Disc using libcdio.
 
-use std::ptr;
 use std::process;
+use std::ptr;
 
-use libcdio_sys::{lsn_t, track_t, CdIo_t, cdio_track_enums_CDIO_CDROM_LEADOUT_TRACK,
-                  driver_id_t_DRIVER_UNKNOWN, CDIO_INVALID_LSN};
-use libcdio_sys::{cdio_destroy, cdio_get_disc_last_lsn, cdio_get_first_track_num,
-                  cdio_get_num_tracks, cdio_get_track_lsn, cdio_open};
+use libcdio_sys::{
+    cdio_destroy, cdio_get_disc_last_lsn, cdio_get_first_track_num, cdio_get_num_tracks,
+    cdio_get_track_lsn, cdio_open,
+};
+use libcdio_sys::{
+    cdio_track_enums_CDIO_CDROM_LEADOUT_TRACK, driver_id_t_DRIVER_UNKNOWN, lsn_t, track_t, CdIo_t,
+    CDIO_INVALID_LSN,
+};
 
 fn main() {
     let p_cdio: *mut CdIo_t = unsafe { cdio_open(ptr::null(), driver_id_t_DRIVER_UNKNOWN) };
@@ -44,7 +48,8 @@ fn main() {
     println!(
         "{:3X}: {} leadout",
         cdio_track_enums_CDIO_CDROM_LEADOUT_TRACK,
-        unsafe { cdio_get_track_lsn(p_cdio, cdio_track_enums_CDIO_CDROM_LEADOUT_TRACK as u8) } as u64
+        unsafe { cdio_get_track_lsn(p_cdio, cdio_track_enums_CDIO_CDROM_LEADOUT_TRACK as u8) }
+            as u64
     );
 
     unsafe { cdio_destroy(p_cdio) };
